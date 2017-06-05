8f0f84e2bc2ff22bb28e13841f9e82b8e246c493
Lightman <yaruk.my@gmail.com>
2017-06-05 15:18:20 +0300

    fix: extend parent props with childs
    It was in reverse

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function(child, parent, parentName) {
 		if (parent._ignoreExtend && parent._ignoreExtend.indexOf(key) != -1) continue;
 		if (key in child) {
 			if (isPlainObject(parent[key]))
-				child[key] = extend(true, child[key], parent[key]);
+				child[key] = extend(true, parent[key], child[key]);
 		} else child[key] = clone(parent[key]);
 	}
 	if (!child.parents) child.parents = {};
